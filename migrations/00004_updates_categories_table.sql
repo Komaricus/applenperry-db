265a79d2016fc9ae0634f7f32c4a05e22583beaa
Komaricus <komarovshura@yandex.ru>
2020-09-17 00:33:02 +0300

    Updates categories table

---
+++ migrations/00004_updates_categories_table.sql
@@ -0,0 +1,8 @@
+-- +goose Up
+alter table dbo.categories alter column id set default uuid_generate_v4();
+
+alter table dbo.categories
+	add created_at timestamp default now() not null;
+
+alter table dbo.categories
+	add is_deleted bool default false not null;
