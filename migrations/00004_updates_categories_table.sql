5ab1ee506ca173c3135701103be66d470bd02b92
Komaricus <komarovshura@yandex.ru>
2020-09-21 20:36:40 +0300

    Adds admins table

--- migrations/00004_updates_categories_table.sql
+++ migrations/00004_updates_categories_table.sql
@@ -1,8 +1,9 @@
 -- +goose Up
-alter table dbo.categories alter column id set default uuid_generate_v4();
+alter table dbo.categories
+    alter column id set default uuid_generate_v4();
 
 alter table dbo.categories
-	add created_at timestamp default now() not null;
+    add created_at timestamp default now() not null;
 
 alter table dbo.categories
-	add is_deleted bool default false not null;
+    add is_deleted bool default false not null;
---
+++ migrations/00005_adds_admins_table.sql
@@ -0,0 +1,13 @@
+-- +goose Up
+create table if not exists dbo.admins
+(
+    id       uuid primary key not null default uuid_generate_v4(),
+    login    varchar(100)     not null,
+    password varchar(64)      not null
+);
+
+alter table dbo.admins
+    owner to appleadmin;
+
+-- +goose Down
+drop table if exists dbo.admins;
